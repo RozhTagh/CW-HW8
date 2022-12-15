96b606359a78cb7c76d230c28d046968010be337
RozhTagh <rozhintn@gmail.com>
2022-12-15 19:14:18 +0330

    CW homework

---
+++ skills.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>رزومه تحصیلی من | مهارت ها</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header class="main-header">
+<nav class="nav">
+<ul>
+<li><a href="cw.php">معرفی</a></li>
+<li><a href="skills.php">مهارت ها و توانمندی ها</a></li>
+<li><a href="achieve.php">افتخارات و دستاوردها</a></li>
+<li><a href="communicate.php">تماس با من</a></li>
+</ul>
+</nav>
+<h1 class="name">رزومه تحصیلی من</h1>
+</header>
+<section>
+<h2 class="section-header">مهارت ها و توانمندی ها</h2>
+<ol class="skills">
+<li class="skill-list">ورزش بدمینتون به مدت پنج سال</li>
+<li class="skill-list">نوازندگی پیانو به مدت ده سال</li>
+<li class="skill-list">آشنایی مقدماتی با مبانی برنامه نویسی به زبان C</li>
+<li class="skill-list">در حال یادگیری برنامه نویسی وب</li>
+</ol>
+</section>
+<footer class="main-footer">
+<img class="footer-image" src="image11.png" width="100" height="100">
+<h3 class="footer-name">ارتباط با من</h3>
+<ul>
+<li><img src = "image5.png" width="25" height="25"></li>
+<a href="mailto:rozhintn@gmail.com">rozhintn@gmail.com</a>
+<li><img src = "image4.ico" width="25" height="25"></li>
+<a href="https://github.com/RozhTagh">github.com/RozhTagh</a>
+<li><img src = "image2.png" width="25" height="25"></li>
+<a href="https://t.me/RozhTagh">t.me/RozhTagh</a>
+<li><img src="image3.ico" width="25" height="25"></li>
+<a href="https://instagram.com/rozhin_tn">instagram.com/rozhin_tn</a>
+<p class="author-name">creator: Rozhin Taghizadegan</p>
+</ul>
+</footer>
+</body>
+</html>
